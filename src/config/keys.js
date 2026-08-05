3858df9100fd05df47aeebdf44c611cffca64903
evankinsey <evankinseyy@gmail.com>
2026-08-05 07:31:35 -0400

    Update Android key for RevenueCat

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,4 +1,4 @@
 export const REVENUECAT_IOS_KEY = 'appl_TdTDZtQDVNVPGbNYTROisxiRrRN';
-export const REVENUECAT_ANDROID_KEY = '';
+export const REVENUECAT_ANDROID_KEY = 'goog_SQhHNhLhdZqXghpiZkvHSsTfcnl';
 export const SUPERWALL_IOS_KEY = '';
 export const SUPERWALL_ANDROID_KEY = '';
